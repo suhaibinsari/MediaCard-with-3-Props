2e093ce9c991170067832bfbad7da8a779b1f7ff
suhaibinsari <mstar390@gmail.com>
2022-06-27 19:59:02 +0500

    update

--- src/components/Mediacard.js
+++ src/components/Mediacard.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import image from '../img/im1.jpeg'
-const Mediacard = ({ title, body, imageURL, altText }) => {
+const Mediacard = ({ title, body, imageURL, altText, style }) => {
   return (
     <div>
       <h2>{title}</h2>
       <p><strong>{body}</strong></p>
-      <img src={image} alt={altText}/>
+      <img src={image} alt={altText} style={{  width:300 }}/>
 
 
 
